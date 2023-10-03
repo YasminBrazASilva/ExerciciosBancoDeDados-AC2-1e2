ff8df1494ca99d33c50d755035f19c14ce005be1
Yasmin Braz A Silva <141586083+YasminBrazASilva@users.noreply.github.com>
2023-10-03 08:48:45 -0300

    Update Exercicio1_12.sql

--- Exercicio1_12.sql
+++ Exercicio1_12.sql
@@ -1,3 +1,3 @@
--- Seleciona animais que possuam peso menor que 30, que nasceram antes de 2012 e que sejam amarelos ou roxos
+-- Seleciona animais que possuam peso menor ou igual que 30, que nasceram antes de 2012 e que sejam amarelos ou roxos
 SELECT * FROM animais WHERE peso <= 30 and cor IN ('amarelo', 'roxo') and nasc > YEAR(2012)
 
